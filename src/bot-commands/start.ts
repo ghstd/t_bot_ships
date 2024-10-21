796f6c7f2e617414699947e20fd87133373761a9
AntonRm <tworkn1@gmail.com>
2024-10-21 11:59:59 +0300

    fix 'start' bot-command

--- src/bot-commands/start.ts
+++ src/bot-commands/start.ts
@@ -5,14 +5,14 @@ import { dbAddUser, dbGetUser } from '../db-queries/queries.js'
 export async function start(ctx: CTX) {
 	console.log('source: ', 'start')
 	const user = await dbGetUser(ctx.from.id)
-	if ('id' in user) {
-		await ctx.reply('нифига, вы уже есть в списке бота', Markup.removeKeyboard())
-	} else {
+	if ( !user || !('id' in user) ) {
 		const newUser: User = {
 			id: ctx.from.id,
 			name: ctx.from.first_name + ' ' + (ctx.from.last_name ? ctx.from.last_name : '')
 		}
 		await dbAddUser(newUser)
 		await ctx.reply('теперь вы добавлены в список бота', Markup.removeKeyboard())
+	} else {
+		await ctx.reply('вы уже есть в списке бота', Markup.removeKeyboard())
 	}
 }
